50e498158d4b02fa3d85e858df982edb16e21f70
valselee <sollleky72@gmail.com>
2023-08-05 22:52:56 +0900

    [Feat] 간단한 List 구현

--- TCAWorkshop/BeforeTCA/Views/ContentView.swift
+++ TCAWorkshop/BeforeTCA/Views/ContentView.swift
@@ -2,20 +2,58 @@
 //  ContentView.swift
 //  BeforeTCA
 //
+//  애플 디벨로퍼 아카데미에 회의실 예약 앱이 있다면?
 //  Created by Celan on 2023/08/04.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject
+    private var meetingRoomListVM: MeetingRoomListViewModel = .init()
+    
+    @State private var isSectionToggled: Bool = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            Section {
+                if meetingRoomListVM.bookedMeetingRoomArray.count > 0 {
+                    ForEach(meetingRoomListVM.bookedMeetingRoomArray) { meetingRoom in
+                        Text("\(meetingRoom.rentBy)")
+                    }
+                } else {
+                    Text("아직 내가 예약한 회의실이 없어요!")
+                }
+                
+            } header: {
+                Text("내가 예약한 회의실")
+            }
+            
+            Section {
+                if isSectionToggled {
+                    Text("?")
+                }
+                
+            } header: {
+                HStack {
+                    Text("회의실 찾아보기")
+                    Spacer()
+                    Button {
+                        withAnimation {
+                            isSectionToggled.toggle()
+                        }
+                        
+                    } label: {
+                        Image(systemName: "chevron.forward")
+                    }
+                    .font(.footnote)
+                    .rotationEffect(isSectionToggled ? .degrees(90) : .zero)
+                }
+            }
         }
-        .padding()
+        .task {
+            await meetingRoomListVM.fetchAllMeetingRoom()
+        }
     }
 }
 
