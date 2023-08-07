e35d6c657459a2ce52c41a365dcea84b262de331
valselee <sollleky72@gmail.com>
2023-08-07 23:27:18 +0900

    [Feat] 1. MeetingRoomFeature에서 firebase 문서를 비동기 생성하는 코드 구현 2. Reducer 자체에서 갖는 Collection, 리스너 선언 3. Equatable == 구현

--- TCAWorkshop/TCAWorkshop/Reducers/MeetingRoomFeature.swift
+++ TCAWorkshop/TCAWorkshop/Reducers/MeetingRoomFeature.swift
@@ -5,11 +5,22 @@
 //  Created by Celan on 2023/08/07.
 //
 
-import Foundation
 import ComposableArchitecture
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Foundation
 
 struct MeetingRoomFeature: Reducer {
+    private let collection = Firestore.firestore().collection("MeetingRooms")
+    
     struct State: Equatable {
+        static func == (lhs: MeetingRoomFeature.State, rhs: MeetingRoomFeature.State) -> Bool {
+            lhs.availableRoomArray == rhs.availableRoomArray &&
+            lhs.bookedMeetingRoomArray == rhs.bookedMeetingRoomArray &&
+            lhs.unavailableMeetingRoomArray == rhs.unavailableMeetingRoomArray
+        }
+        
+        var firebaseListener: ListenerRegistration?
         var availableRoomArray: [MeetingRoom] = []
         var unavailableMeetingRoomArray: [MeetingRoom] = []
         var bookedMeetingRoomArray: [MeetingRoom] = []
@@ -17,18 +28,55 @@ struct MeetingRoomFeature: Reducer {
     
     @frozen enum Action: Equatable {
         case meetingRoomCellTapped
+        case onMeetingRoomListViewAppear
+        case makeMeetingRoomButtonTapped
     }
     
-    var body: some ReducerOf<MeetingRoomFeature> {
-        Reduce { state, action in
-            switch action {
-            case .meetingRoomCellTapped:
-                
-                return .run { send in
-                    await send(.meetingRoomCellTapped)
+    public func reduce(
+        into state: inout State,
+        action: Action
+    ) -> Effect<Action> {
+        switch action {
+        case .meetingRoomCellTapped:
+            
+            return .none
+            
+        case .onMeetingRoomListViewAppear:
+            
+            return .run { send in
+                let querySnapshot = try await collection.getDocuments()
+                for doc in querySnapshot.documents {
+                    // TODO: 비동기로 각 배열에 fetched된 meetingRoom을 추가해주세요
                 }
             }
+            
+        case .makeMeetingRoomButtonTapped:
+            let newMeetingRoom = MeetingRoom(
+                id: .init(),
+                meetingRoomName: "Test",
+                rentDate: .now,
+                rentBy: Constants.DUMMY_MEETINGROOM_RENTNAMES.randomElement()!
+            )
+            
+            return .run { send in
+                /// <NSThread: 0x600000b96080>{number = 6, name = (null)}
+                /// <NSThread: 0x600000bd1300>{number = 10, name = (null)}
+                /// .run의 클로저는 그 자체로 Task를 생성하여 동시성 작업을 실행한다.
+                try await self.collection
+                    .document(newMeetingRoom.id.uuidString)
+                    .setData([
+                        "id": newMeetingRoom.id.uuidString,
+                        "rentDate": Timestamp(date: newMeetingRoom.rentDate),
+                        "rentBy": newMeetingRoom.rentBy,
+                        "meetingRoomName": newMeetingRoom.meetingRoomName
+                    ])
+            }
         }
+    }
+    
+    @MainActor
+    private func appendValues(_ state: inout State) async {
+        
     }
 }
 
