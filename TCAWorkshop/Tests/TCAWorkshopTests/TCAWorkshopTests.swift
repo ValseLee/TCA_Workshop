2474539ad6b01390b3dc081f14e64507c9cf4ab3
valselee <sollleky72@gmail.com>
2023-08-10 17:44:04 +0900

    [Test] 1. 기초 테스트 작성, State가 변하지 않으면 변하지 않아서 에러가 생길 수 있음

--- TCAWorkshop/Tests/TCAWorkshopTests/TCAWorkshopTests.swift
+++ TCAWorkshop/Tests/TCAWorkshopTests/TCAWorkshopTests.swift
@@ -6,8 +6,24 @@
 //
 
 import XCTest
+import ComposableArchitecture
+import FirebaseSharedSwift
+import Firebase
 @testable import TCAWorkshop
 
+@MainActor
 final class TCAWorkshopTests: XCTestCase {
-    
+    let store = TestStore(initialState: MeetingRoomFeature.State()) {
+        MeetingRoomFeature()
+    }
+
+    func testFetchResponse() async throws {
+        await store.send(.onMeetingRoomListViewAppear) {
+            $0.isMeetingRoomFetching = true
+        }
+        
+        await store.send(.meetingRoomFetchResponse) {
+            $0.isMeetingRoomFetching = false
+        }
+    }
 }
