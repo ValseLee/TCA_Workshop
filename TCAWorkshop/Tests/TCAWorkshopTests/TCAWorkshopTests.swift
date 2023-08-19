dccf82ae6c28d7ca2e29fe550bc0fbef5e8e614d
valselee <sollleky72@gmail.com>
2023-08-19 17:28:29 +0900

    [Feat] 1. State 테스트 코드 작성 2. Side Effect 테스트 코드 작성(객체 분리가 되어 잇지 않아서 MicroFeature Reducer 구현해서 테스트 재구성)

--- TCAWorkshop/Tests/TCAWorkshopTests/TCAWorkshopTests.swift
+++ TCAWorkshop/Tests/TCAWorkshopTests/TCAWorkshopTests.swift
@@ -13,17 +13,47 @@ import Firebase
 
 @MainActor
 final class TCAWorkshopTests: XCTestCase {
-    let store = TestStore(initialState: MeetingRoomFeature.State()) {
-        MeetingRoomFeature()
+    let store = TestStore(initialState: MeetingRoomListDomain.State()) {
+        MeetingRoomListDomain()
+    } withDependencies: {
+        $0.uuid = .incrementing
+        $0.date = .constant(Date())
     }
 
-    func testFetchResponse() async throws {
+    func testFetchResponseForLastDoc() async throws {
         await store.send(.onMeetingRoomListViewAppear) {
             $0.isMeetingRoomFetching = true
         }
         
-        await store.send(.meetingRoomFetchResponse) {
+        let querySnapshot = try await Constants.FIREBASE_COLLECTION.getDocuments()
+        if let lastDoc = querySnapshot.documents.last {
+            let fetchedMeetingRoom = try lastDoc.data(as: MeetingRoom.self)
+            let meetingRoomDomainState = MeetingRoomDomain.State(
+                rentDate: self.store.dependencies.date.now,
+                id: fetchedMeetingRoom.id,
+                selectedMeetingRoom: fetchedMeetingRoom
+            )
+            
+            await store.send(.processFetchedMeetingRoomIntoState(with: fetchedMeetingRoom)) {
+                switch fetchedMeetingRoom.rentBy {
+                case "CURRENT_USER":
+                    $0.bookedMeetingRoomArray = [meetingRoomDomainState]
+                case "AVAILABLE":
+                    $0.availableMeetingRoomArray = [meetingRoomDomainState]
+                case "OTHERS":
+                    $0.unavailableMeetingRoomArray = [meetingRoomDomainState]
+                default:
+                    fatalError("meetingRoomDomainState?")
+                }
+            }
+        }
+    }
+    
+    func testMeetingRoomArrays() async throws {
+        await store.send(.meetingRoomFetchComplete) {
             $0.isMeetingRoomFetching = false
+            $0.isMeetingRoomFetchFailed = false
+            $0.isMeetingRoomInitOnce = true
         }
     }
 }
