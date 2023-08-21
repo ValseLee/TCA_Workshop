9e744f93cb700d8d22ba9ba40a975c310accd61c
valselee <sollleky72@gmail.com>
2023-08-21 21:13:54 +0900

    [Feat] 1. View+ button Label Modifier 구현

--- TCAWorkshop/Common/Extensions/View+ButtonLabel.swift
+++ TCAWorkshop/Common/Extensions/View+ButtonLabel.swift
@@ -5,4 +5,21 @@
 //  Created by Celan on 2023/08/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct ButtonLabelModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .bold()
+            .foregroundColor(.primary)
+            .padding(.vertical, 8)
+            .frame(maxWidth: .infinity)
+            .shadow(radius: 10)
+    }
+}
+
+extension View {
+    public func makeButtonLabelWithStyle() -> some View {
+        modifier(ButtonLabelModifier())
+    }
+}
