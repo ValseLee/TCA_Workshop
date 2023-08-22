f0adf8d17a2cda4317f6b8a21c4fd21648d6247e
valselee <sollleky72@gmail.com>
2023-08-22 23:47:01 +0900

    [Feat] 1. fetch, update를 정의하는 protocol 선언 2. 해당 protocol을 채택하는 client 생성, fetch, update를 @Sendable 타입으로 구 현 3. live, test로 구분

--- TCAWorkshop/Common/Firebase/FirebaseClient.swift
+++ TCAWorkshop/Common/Firebase/FirebaseClient.swift
@@ -5,4 +5,56 @@
 //  Created by Celan on 2023/08/22.
 //
 
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 import Foundation
+
+protocol FirebaseInterface {
+    associatedtype FirebaseData: Codable
+    
+    var update: @Sendable (_ with: FirebaseData) async throws -> Void { get }
+    var fetch: @Sendable () async throws -> [FirebaseData] { get }
+}
+
+struct MeetingRoomClient: FirebaseInterface {
+    var update: (@Sendable (_ with: MeetingRoom) async throws -> Void)
+    var fetch: (@Sendable () async throws -> [MeetingRoom])
+}
+
+extension MeetingRoomClient {
+    static let live = Self(
+    update: { meetingRoom in
+        Constants.FIREBASE_COLLECTION
+            .document(meetingRoom.id.uuidString)
+            .setData(
+                [
+                    "rentDate": Timestamp(date: meetingRoom.rentDate),
+                    "rentBy": meetingRoom.rentBy,
+                    "rentHourAndMinute": meetingRoom.rentHourAndMinute,
+                ],
+                merge: true
+            )
+    }, fetch: {
+        var fetchedMeetingRooms = [MeetingRoom]()
+                
+        async let docSnapshot = try Constants.FIREBASE_COLLECTION
+            .getDocuments()
+        
+        for doc in try await docSnapshot.documents {
+            let fetchedMeetingRoom = try doc.data(as: MeetingRoom.self)
+            /// 굳이 store의 action을 여기서 억지로 처리할 필요가 있을까
+            /// 'fetch' 이상의 역할을 하게 되는데 왜?
+            fetchedMeetingRooms.append(fetchedMeetingRoom)
+        }
+        
+        return fetchedMeetingRooms
+    })
+
+    static let test = Self(
+        update: { meetingRoom in
+            print("\(meetingRoom.id), Update")
+        }, fetch: {
+            print("Fetch Start in: \(Constants.FIREBASE_COLLECTION.description)")
+            return [MeetingRoom.testInstance()]
+        })
+}
