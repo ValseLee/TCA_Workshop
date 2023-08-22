9cbd89c364a00c4f04753705f39a0433d30bc277
valselee <sollleky72@gmail.com>
2023-08-23 02:19:31 +0900

    [Feat] 1. Child에서 특정 미팅룸의 예약 상태가 바뀔 경우, onViewAppear가 조건에 따라 추가 작업을 실시 2. Parent의 action 호출 처리가 먼저 이루어지기 때문에 배열에서 지우는 로직 대신 배열 전체를 조건여하에 따라 전부 지우고 ㅅ다시 fetch 하도록 구 3. FirebaseClient 에 따라 코드 일부 수정

--- TCAWorkshop/TCAWorkshop/Root/MeetingRoomListDomain.swift
+++ TCAWorkshop/TCAWorkshop/Root/MeetingRoomListDomain.swift
@@ -11,6 +11,7 @@ import FirebaseFirestoreSwift
 import Foundation
 
 struct MeetingRoomListDomain: Reducer {
+    let firebaseClient: MeetingRoomClient = .live
     @Dependency(\.date.now) var now
     
     struct State: Equatable {
@@ -22,6 +23,8 @@ struct MeetingRoomListDomain: Reducer {
         var isMeetingRoomFetching: Bool = false
         var isMeetingRoomFetchFailed: Bool = false
         var isFetchAvailable: Bool = true
+        
+        var hasMeetingRoomChanged: Bool = false
         
         var isAvailableMeetingRoomArrayEmpty: Bool = true
         var isUnavailableMeetingRoomArrayEmpty: Bool = true
@@ -43,11 +46,13 @@ struct MeetingRoomListDomain: Reducer {
             id: UnavabilableMeetingRoomFeature.State.ID,
             action: UnavabilableMeetingRoomFeature.Action
         )
-        
+                
         case onMeetingRoomListViewAppear
-        case processFetchedMeetingRoomIntoState(with: MeetingRoom)
+        case processFetchedMeetingRooms(with: [MeetingRoom])
+        
         case meetingRoomFetchComplete
         case meetingRoomFetchFailed
+        
         case listRefreshed
         case confirmationDialogRetryButtonTapped
         case confirmationDialogDismissed(Bool)
@@ -58,27 +63,59 @@ struct MeetingRoomListDomain: Reducer {
         Reduce { state, action in
             switch action {
                 // MARK: - .forEach
-            case .bookedMeetingRoom:
+            case let .bookedMeetingRoom(_, action):
+                /// + Child의 액션보다 Parent가 받는 액션이 먼저 수행된다.
+                if case .cancelReservationResponse = action {
+                    state.hasMeetingRoomChanged = true
+                }
+                
                 return .none
                 
-            case .availableMeetingRoom:
+            case let .availableMeetingRoom(_, action):
+                if case .reservationResponse = action {
+                    state.hasMeetingRoomChanged = true
+                }
+                
                 return .none
                 
             case .unavailableMeetingRoom:
                 return .none
-                
+                    
                 // MARK: - Render
             case .onMeetingRoomListViewAppear:
                 checkIfMeetingRoomArrayIsEmpty(&state)
                 
                 guard state.isMeetingRoomInitOnce else {
                     state.isMeetingRoomFetching = true
-                    return .run {
-                        try await Task.sleep(for: .seconds(0.5))
-                        try await fetchMeetingRooms(send: $0)
-                    }
-                    catch: { error, send in
+                    return .run { send in
+                        let start = Date()
+                        defer { Logger.methodExecTimePrint(start) }
+                        
+                        let fetchedMeetingRooms = try await firebaseClient.fetch()
+                        await send(
+                            .processFetchedMeetingRooms(with: fetchedMeetingRooms),
+                            animation: .easeInOut
+                        )
+                    } catch: { error, send in
                         print("FETCH FAILED: ", error)
+                        await send(.meetingRoomFetchFailed, animation: .easeInOut)
+                    }
+                }
+                
+                if state.hasMeetingRoomChanged {
+                    state.isMeetingRoomFetching = true
+                    state.availableMeetingRoomArray.removeAll()
+                    state.bookedMeetingRoomArray.removeAll()
+                    state.unavailableMeetingRoomArray.removeAll()
+                    
+                    return .run { send in
+                        let fetchedMeetingRooms = try await firebaseClient.fetch()
+                        await send(
+                            .processFetchedMeetingRooms(with: fetchedMeetingRooms),
+                            animation: .easeInOut
+                        )
+                    } catch: { error, send in
+                        print("UPDATE FETCH FAILED: ", error)
                         await send(.meetingRoomFetchFailed, animation: .easeInOut)
                     }
                 }
@@ -87,23 +124,26 @@ struct MeetingRoomListDomain: Reducer {
                 
             case .listRefreshed:
                 state.isMeetingRoomFetching = true
-                return .run {
-                    try await Task.sleep(for: .seconds(0.5))
-                    try await fetchMeetingRooms(send: $0)
-                }
-                catch: { error, send in
+                return .run { send in
+                    let fetchedMeetingRooms = try await self.firebaseClient.fetch()
+                    await send(
+                        .processFetchedMeetingRooms(with: fetchedMeetingRooms),
+                        animation: .easeInOut
+                    )
+                } catch: { error, send in
                     print("REFRESH FAILED: ", error)
                     await send(.meetingRoomFetchFailed, animation: .easeInOut)
                 }
-                
-            case let .processFetchedMeetingRoomIntoState(fetchedMeetingRoom):
-                self.makeMeetingRoom(&state, with: fetchedMeetingRoom)
-                return .none
+                                
+            case let .processFetchedMeetingRooms(fetchedMeetingRooms):
+                self.makeMeetingRoom(&state, with: fetchedMeetingRooms)
+                return .send(.meetingRoomFetchComplete, animation: .easeInOut)
                 
             case .meetingRoomFetchComplete:
                 state.isMeetingRoomFetching = false
                 state.isMeetingRoomFetchFailed = false
                 state.isMeetingRoomInitOnce = true
+                state.hasMeetingRoomChanged = false
                 checkIfMeetingRoomArrayIsEmpty(&state)
                 
                 return .none
@@ -118,9 +158,10 @@ struct MeetingRoomListDomain: Reducer {
                 state.isMeetingRoomFetching = true
                 state.isMeetingRoomFetchFailed = false
                 
-                return .run {
+                return .run { send in
+                    let fetchedMeetingRooms = try await self.firebaseClient.fetch()
                     try await Task.sleep(for: .seconds(0.5))
-                    try await fetchMeetingRooms(send: $0)
+                    await send(.meetingRoomFetchComplete, animation: .easeInOut)
                 }
                 catch: { error, send in
                     print("FETCH FAILED AGAIN: ", error)
@@ -160,56 +201,57 @@ struct MeetingRoomListDomain: Reducer {
     // MARK: - METHODS
     private func makeMeetingRoom(
         _ state: inout State,
-        with fetchedMeetingRoom: MeetingRoom
+        with fetchedMeetingRooms: [MeetingRoom]
     ) {
-        switch fetchedMeetingRoom.rentBy {
-        case "CURRENT_USER":
-            if let idx = state.bookedMeetingRoomArray.firstIndex(
-                where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
-            ) {
-                state.bookedMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
-            } else {
-                state.bookedMeetingRoomArray.append(
-                    BookedMeetingRoomFeature.State(
-                        selectedMeetingRoom: fetchedMeetingRoom,
-                        id: fetchedMeetingRoom.id
+        fetchedMeetingRooms.forEach { fetchedMeetingRoom in
+            switch fetchedMeetingRoom.rentBy {
+            case "CURRENT_USER":
+                if let idx = state.bookedMeetingRoomArray.firstIndex(
+                    where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
+                ) {
+                    state.bookedMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
+                } else {
+                    state.bookedMeetingRoomArray.append(
+                        BookedMeetingRoomFeature.State(
+                            selectedMeetingRoom: fetchedMeetingRoom,
+                            id: fetchedMeetingRoom.id
+                        )
                     )
-                )
-            }
-            
-        case "OTHERS":
-            if let idx = state.unavailableMeetingRoomArray.firstIndex(
-                where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
-            ) {
-                state.unavailableMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
-            } else {
-                state.unavailableMeetingRoomArray.append(
-                    UnavabilableMeetingRoomFeature.State(
-                        selectedMeetingRoom: fetchedMeetingRoom,
-                        id: fetchedMeetingRoom.id
+                }
+                
+            case "OTHERS":
+                if let idx = state.unavailableMeetingRoomArray.firstIndex(
+                    where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
+                ) {
+                    state.unavailableMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
+                } else {
+                    state.unavailableMeetingRoomArray.append(
+                        UnavabilableMeetingRoomFeature.State(
+                            selectedMeetingRoom: fetchedMeetingRoom,
+                            id: fetchedMeetingRoom.id
+                        )
                     )
-                )
-            }
-            
-        case "AVAILABLE":
-            if let idx = state.availableMeetingRoomArray.firstIndex(
-                where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
-            ) {
-                state.availableMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
-            } else {
-                state.availableMeetingRoomArray.append(
-                    AvailableMeetingRoomFeature.State(
-                        selectedMeetingRoom: fetchedMeetingRoom,
-                        id: fetchedMeetingRoom.id
+                }
+                
+            case "AVAILABLE":
+                if let idx = state.availableMeetingRoomArray.firstIndex(
+                    where: { $0.selectedMeetingRoom == fetchedMeetingRoom }
+                ) {
+                    state.availableMeetingRoomArray[idx].selectedMeetingRoom = fetchedMeetingRoom
+                } else {
+                    state.availableMeetingRoomArray.append(
+                        AvailableMeetingRoomFeature.State(
+                            selectedMeetingRoom: fetchedMeetingRoom,
+                            id: fetchedMeetingRoom.id
+                        )
                     )
-                )
+                }
+                
+            default:
+                return
             }
-            
-        default:
-            return
         }
     }
-    
     
     /// Fetch된 MeetingRoom 배열이 비어있는지 확인합니다.
     /// - Parameter state: inout으로 mutate 할 현재 State
@@ -222,19 +264,6 @@ struct MeetingRoomListDomain: Reducer {
         
         if state.bookedMeetingRoomArray.isEmpty { state.isBookedMeetingRoomArrayEmpty = true }
         else { state.isBookedMeetingRoomArrayEmpty = false }
-    }
-    
-    private func fetchMeetingRooms(send: Send<MeetingRoomListDomain.Action>) async throws {
-        let start = Date()
-        defer { Logger.methodExecTimePrint(start) }
-        
-        let querySnapshot = try await Constants.FIREBASE_COLLECTION.getDocuments()
-        for doc in querySnapshot.documents {
-            let fetchedMeetingRoom = try doc.data(as: MeetingRoom.self)
-            await send(.processFetchedMeetingRoomIntoState(with: fetchedMeetingRoom))
-        }
-        
-        await send(.meetingRoomFetchComplete, animation: .easeInOut)
     }
 }
 
