efbc8d60fd8bebdcda303418fdcc70525cb1b820
valselee <sollleky72@gmail.com>
2023-08-23 02:14:16 +0900

    [Chore] 1. DateFormatter의 conv init 구현 2. 상수형 추가 선언(cancelable id)

--- TCAWorkshop/Common/Extensions/DateFormatter+Init.swift
+++ TCAWorkshop/Common/Extensions/DateFormatter+Init.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension DateFormatter {
+    /**
+     dateFormat, Calendar를 기본 값으로 설정하여 DateFormatter를 초기화하는 생성자
+    - Parameters:
+        - dateFormat: "D", "EEE" 형태의 문자열 포맷
+        - calendar: 주어진 달력을 기준으로 Formatter 생성
+     */
+    convenience init(
+        dateFormat: String,
+        calendar: Calendar = .current
+    ) {
+        self.init()
+        self.dateFormat = dateFormat
+        self.calendar = calendar
+    }
+}
