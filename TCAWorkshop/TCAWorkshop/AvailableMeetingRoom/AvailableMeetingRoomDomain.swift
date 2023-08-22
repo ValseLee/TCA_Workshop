9204271312ac9472ba42e58c76350a27516678d7
valselee <sollleky72@gmail.com>
2023-08-23 02:17:44 +0900

    [Feat] 1. Micro Feature 단위로 뷰, 리듀서 분리 및 구현 2. 각 Feature는 Feature에 해당하는 기능과 State만을 가지며 unavilable 상태의 경우 그 어떤 액션도 필요 없음 3. view Disappear 시점에 기존 State를 원상태로 init 4. cancellable 한 비동기 코드로 정 리 5. MeetingRoomClient 로 편리하게 모든 리듀서에서 fetch 및 update 가능하도록 static instance 를 불러와서 메소드 호출

--- TCAWorkshop/TCAWorkshop/AvailableMeetingRoom/AvailableMeetingRoomDomain.swift
+++ TCAWorkshop/TCAWorkshop/AvailableMeetingRoom/AvailableMeetingRoomDomain.swift
@@ -9,6 +9,8 @@ import ComposableArchitecture
 import Foundation
 
 struct AvailableMeetingRoomFeature: Reducer {
+    let meetingRoomClient: MeetingRoomClient = .live
+    
     struct State: Equatable, Identifiable {
         @BindingState var selectedMeetingRoom: MeetingRoom
         var id: UUID
@@ -19,12 +21,9 @@ struct AvailableMeetingRoomFeature: Reducer {
     
     enum Action: Equatable, BindableAction {
         case binding(BindingAction<State>)
-        case onMeetingRoomViewAppear
-        
+        case onViewDisappear
         case reservationButtonTapped
         case reservationResponse
-        case rentDatePicked(Date)
-        case rentHourAndMinutePicked(Int)
     }
     
     
@@ -36,21 +35,32 @@ struct AvailableMeetingRoomFeature: Reducer {
             case .binding:
                 return .none
                 
-            case .onMeetingRoomViewAppear:
-                return .none
+            case .onViewDisappear:
+                state.isReservationCompleted = false
+                state.isReservationButtonTapped = false
+                return .cancel(id: Constants.CANCELABLE_RESERVATION_ID)
                 
             case .reservationButtonTapped:
-                return .none
+                guard state.isReservationCompleted else {
+                    state.isReservationButtonTapped = true
+                    state.selectedMeetingRoom.rentBy = "CURRENT_USER"
+                    /// 현 시점의 State를 immutable하게 capture copy해서 전달한다.
+                    /// inout value type 의 Data race를 방지
+                    return .run { [selectedMeetingRoom = state.selectedMeetingRoom] send in
+                        try await self.meetingRoomClient.update(selectedMeetingRoom)
+                        try! await Task.sleep(for: .seconds(0.5))
+                        await send(.reservationResponse, animation: .easeInOut)
+                    } catch: { error, send in
+                        print("UPDATE FAILED", error.localizedDescription)
+                    }
+                        .cancellable(id: Constants.CANCELABLE_RESERVATION_ID)
+                }
+                
+                return .send(.reservationResponse, animation: .easeInOut)
                 
             case .reservationResponse:
-                return .none
-                
-            case let .rentDatePicked(date):
-                state.selectedMeetingRoom.rentDate = date
-                return .none
-                
-            case let .rentHourAndMinutePicked(time):
-                state.selectedMeetingRoom.rentHourAndMinute = time
+                state.isReservationButtonTapped = false
+                state.isReservationCompleted = true
                 return .none
             }
         }
--- TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomDomain.swift
+++ TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomDomain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 struct UnavabilableMeetingRoomFeature: Reducer {
     struct State: Equatable, Identifiable {
+        var minuteHourformatter: DateFormatter = DateFormatter(dateFormat: "HH:mm a", calendar: .autoupdatingCurrent)
+        var dayformatter: DateFormatter = DateFormatter(dateFormat: "YY년 MM월 dd일")
+        
         var selectedMeetingRoom: MeetingRoom
         var id: UUID
     }
