f957d4f95a555f9a26f24e30b7e5dc98ab48689c
valselee <sollleky72@gmail.com>
2023-08-22 11:27:44 +0900

    [Feat] 1. 예약할 수 없는 미팅룸 뷰와 리듀서 구현 2. 내가 에약한 미팅룸이 아니라면 예약할 수 없다

--- TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomDomain.swift
+++ TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomDomain.swift
@@ -5,4 +5,22 @@
 //  Created by Celan on 2023/08/21.
 //
 
+import ComposableArchitecture
 import Foundation
+
+struct UnavabilableMeetingRoomFeature: Reducer {
+    struct State: Equatable, Identifiable {
+        var selectedMeetingRoom: MeetingRoom
+        var id: UUID
+    }
+    
+    enum Action: Equatable { }
+    
+    func reduce(
+        into state: inout State,
+        action: Action
+    ) -> Effect<Action> {
+        return .none
+    }
+}
+
--- TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomView.swift
+++ TCAWorkshop/TCAWorkshop/UnavailableMeetingRoom/UnavailableMeetingRoomView.swift
@@ -5,16 +5,152 @@
 //  Created by Celan on 2023/08/21.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 struct UnavailableMeetingRoomView: View {
+    let store: StoreOf<UnavabilableMeetingRoomFeature>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        WithViewStore(
+            self.store,
+            observe: { $0.selectedMeetingRoom }
+        ) { selectedMeetingRoom in
+            VStack(spacing: 24) {
+                Image(systemName: "calendar")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .padding(.vertical)
+                    .overlay(alignment: .topTrailing) {
+                        Image(systemName: "checkmark.circle")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .offset(x: 20)
+                            .foregroundColor(.green)
+                    }
+                
+                Divider()
+                                
+                HStack {
+                    Text("회의실 이름")
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Text(selectedMeetingRoom.meetingRoomName)
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background {
+                            Capsule()
+                                .foregroundColor(.green)
+                        }
+                }
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                
+                HStack {
+                    Text("회의실 대여자")
+                        .bold()
+                    
+                    Spacer()
+                    
+                    Text(selectedMeetingRoom.rentBy)
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background {
+                            Capsule()
+                                .foregroundColor(.green)
+                        }
+                }
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+                
+                DatePicker(
+                    selection: .constant(selectedMeetingRoom.rentDate),
+                    in: Date()...,
+                    displayedComponents: .date
+                ) {
+                    Text("대여 희망일")
+                        .bold()
+                }
+                .environment(\.locale, .current)
+                .datePickerStyle(.compact)
+                
+                DatePicker(
+                    selection: .constant(selectedMeetingRoom.rentDate),
+                    in: Date()...,
+                    displayedComponents: .hourAndMinute
+                ) {
+                    Text("대여 시작 시간")
+                        .bold()
+                }
+                .environment(\.locale, .current)
+                .datePickerStyle(.graphical)
+                
+                HStack {
+                    VStack(
+                        alignment: .leading,
+                        spacing: 8
+                    ) {
+                        Text("**대여 시간:** ") +
+                        Text("**\(selectedMeetingRoom.rentHourAndMinute) 시간**")
+                            .bold()
+                            .monospacedDigit()
+                            .foregroundColor(.green)
+                        
+                        Text("최대 3시간(베타는 종일 대여)")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                    
+                    Spacer()
+                    
+                    Stepper(
+                        value: .constant(selectedMeetingRoom.rentHourAndMinute),
+                        in: 1...3
+                    ) {
+                        // Text("LABLES HIDDEN")
+                    }
+                    .labelsHidden()
+                }
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .leading
+                )
+
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Text("크킥")
+                }
+            }
+            .padding()
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .top
+            )
+        }
     }
 }
 
 struct UnavailableMeetingRoomView_Previews: PreviewProvider {
     static var previews: some View {
-        UnavailableMeetingRoomView()
+        UnavailableMeetingRoomView(
+            store: Store(
+                initialState: UnavabilableMeetingRoomFeature.State(
+                    selectedMeetingRoom: .testInstance(),
+                    id: .init(0)
+                ), reducer: {
+                    UnavabilableMeetingRoomFeature()
+                }
+            )
+        )
     }
 }
