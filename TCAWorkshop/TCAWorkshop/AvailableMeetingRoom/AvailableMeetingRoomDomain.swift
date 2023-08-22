36fda5b7a231e355dbd1cd803826d900eed5a042
valselee <sollleky72@gmail.com>
2023-08-22 11:26:52 +0900

    [Feat] 1. AvailableMeetingRoom 뷰와 리듀서 구현 중 2. 각 배열은 서로 다른 state를 참조하며 서로 다른 기능을 쓰기 때문에 서로 분리하여 micro 하게 구현 시도

--- TCAWorkshop/TCAWorkshop/AvailableMeetingRoom/AvailableMeetingRoomDomain.swift
+++ TCAWorkshop/TCAWorkshop/AvailableMeetingRoom/AvailableMeetingRoomDomain.swift
@@ -5,4 +5,55 @@
 //  Created by Celan on 2023/08/21.
 //
 
+import ComposableArchitecture
 import Foundation
+
+struct AvailableMeetingRoomFeature: Reducer {
+    struct State: Equatable, Identifiable {
+        @BindingState var selectedMeetingRoom: MeetingRoom
+        var id: UUID
+        
+        var isReservationButtonTapped: Bool = false
+        var isReservationCompleted: Bool = false
+    }
+    
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+        case onMeetingRoomViewAppear
+        
+        case reservationButtonTapped
+        case reservationResponse
+        case rentDatePicked(Date)
+        case rentHourAndMinutePicked(Int)
+    }
+    
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        
+        Reduce<State, Action> { state, action in
+            switch action {
+            case .binding:
+                return .none
+                
+            case .onMeetingRoomViewAppear:
+                return .none
+                
+            case .reservationButtonTapped:
+                return .none
+                
+            case .reservationResponse:
+                return .none
+                
+            case let .rentDatePicked(date):
+                state.selectedMeetingRoom.rentDate = date
+                return .none
+                
+            case let .rentHourAndMinutePicked(time):
+                state.selectedMeetingRoom.rentHourAndMinute = time
+                return .none
+            }
+        }
+    }
+}
+
