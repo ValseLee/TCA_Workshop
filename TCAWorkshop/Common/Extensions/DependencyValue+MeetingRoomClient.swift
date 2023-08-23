a06ed96e462c2b579259486dc69f006329d8fe09
valselee <sollleky72@gmail.com>
2023-08-23 17:23:30 +0900

    [Feat] 1. MeetingRoomClient가 Dependency로 구현되어 test에서도 일관적으로 활용 가능 2. Model의 testInstance를 0으로 init

--- TCAWorkshop/Common/Extensions/DependencyValue+MeetingRoomClient.swift
+++ TCAWorkshop/Common/Extensions/DependencyValue+MeetingRoomClient.swift
@@ -5,4 +5,16 @@
 //  Created by Celan on 2023/08/23.
 //
 
-import Foundation
+import ComposableArchitecture
+
+extension DependencyValues {
+    var meetingRoomClient: MeetingRoomClient {
+        get { self[FirebaseClientKey.self] }
+        set { self[FirebaseClientKey.self] = newValue }
+    }
+}
+
+fileprivate enum FirebaseClientKey: DependencyKey {
+    static var liveValue: MeetingRoomClient = .live
+    static var testValue: MeetingRoomClient = .test
+}
