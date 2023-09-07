84d24ff2b52b13a1d158c1c14371a6abdddbfb35
valselee <sollleky72@gmail.com>
2023-09-07 15:39:24 +0900

    [Feat] 1. 더미 JSON API 통신이 가능하도록 Client 구현 2. URLSession을 활용한 비동기 통신 구현 3. 테스트를 해봐야 하는데 main에 db.json을 먼저 꽂아야 해서 머지 이후에 구현 확 인

--- TCAWorkshop/Common/Network/MeetingRoomClient.swift
+++ TCAWorkshop/Common/Network/MeetingRoomClient.swift
@@ -22,10 +22,21 @@ struct MeetingRoomClient: APINetworkInterface {
 extension MeetingRoomClient {
     enum MeetingRoomClientError: Error {
         case fetchError
+        case postError
     }
     
     static let live = Self(
     update: { meetingRoom in
+        let updatePayload = try JSONEncoder().encode(meetingRoom)
+        let url = URL(string: "https://my-json-server.typicode.com/ValseLee/TCA_Workshop/meetingRooms")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let (data, response) = try await URLSession.shared.upload(for: request, from: updatePayload)
+        
+        if let httpResponse = response as? HTTPURLResponse,
+           httpResponse.statusCode != 200 { throw MeetingRoomClientError.postError }
+        Logger._printData(.update, data)
         
     }, fetch: {
         let (data, response) = try await URLSession.shared.data(
@@ -33,7 +44,10 @@ extension MeetingRoomClient {
         )
         
         guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else { throw MeetingRoomClientError.fetchError }
+              httpResponse.statusCode == 200 else {
+            print("Fetch Failed", response)
+            throw MeetingRoomClientError.fetchError
+        }
         
         let result = try JSONDecoder().decode([MeetingRoom].self, from: data)
         
