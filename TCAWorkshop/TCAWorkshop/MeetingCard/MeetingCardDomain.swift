2353fa21509b4352e6f5687fd05526949cdbb941
valselee <sollleky72@gmail.com>
2023-09-19 23:04:45 +0900

    [Feat] 1. Textfield binding 구현 2. 글자수에 따라 trigger 되는 flag 구현

--- TCAWorkshop/TCAWorkshop/MeetingCard/MeetingCardDomain.swift
+++ TCAWorkshop/TCAWorkshop/MeetingCard/MeetingCardDomain.swift
@@ -7,6 +7,7 @@
 
 import ComposableArchitecture
 import Foundation
+import SwiftUI
 
 struct MeetingCardDomain: Reducer {
     @Dependency(\.meetingRoomClient)
@@ -18,21 +19,54 @@ struct MeetingCardDomain: Reducer {
     struct State: Equatable {
         var id: UUID
         var meetingInfo: MeetingInfo
+        var isSubjectEditButtonTapped: Bool = false
+        var isSubjectMaximumCharcterReached: Bool = false
     }
     
     enum Action: Equatable {
         case cancelButtonTapped
+        case subjectEditButtonTapped
+        case isSubjectEditing(String)
+        case emptySubjectTextFieldButtonTapped
     }
     
     var body: some ReducerOf<Self> {
         core()
+            ._printChanges()
     }
     
     private func core() -> Reduce<State, Action> {
-        Reduce { state, action in
+        Reduce<State, Action> { state, action in
             switch action {
             case .cancelButtonTapped:
+                state.isSubjectEditButtonTapped = false
+                state.isSubjectMaximumCharcterReached = false
                 return .run { _ in await self.dismiss() }
+                
+            case .subjectEditButtonTapped:
+                state.isSubjectEditButtonTapped.toggle()
+                
+                if state.isSubjectMaximumCharcterReached,
+                   state.meetingInfo.subject.count < 20 {
+                    state.isSubjectMaximumCharcterReached = false
+                }
+                return .none
+                
+            case let .isSubjectEditing(newSubject):
+                if newSubject.count > 20 {
+                    state.isSubjectMaximumCharcterReached = true
+                } else if newSubject.count <= 20 {
+                    state.isSubjectMaximumCharcterReached = false
+                    state.meetingInfo.subject = newSubject
+                }
+                
+                return .none
+                
+            case .emptySubjectTextFieldButtonTapped:
+                state.meetingInfo.subject = ""
+                state.isSubjectMaximumCharcterReached = false
+                
+                return .none
             }
         }
     }
