e60cef90a85c661fa0a293ff89b434e2663b1d65
valselee <sollleky72@gmail.com>
2023-09-25 22:25:00 +0900

    [Feat] 1. protocol이 새로운 속성을 갖는다(근데 프로토콜 의존성 결합 때문에 골치좀 썩힘) 2. GuessAge API 호출을 위한 client 구현

--- TCAWorkshop/Common/Network/APINetworkProtocol.swift
+++ TCAWorkshop/Common/Network/APINetworkProtocol.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol APINetworkInterface {
+    associatedtype APIData: Codable
+    associatedtype FetchKey: Codable
+    
+    var update: @Sendable (_ updateTarget: APIData) async throws -> Void { get }
+    var fetchDataArray: @Sendable () async throws -> [APIData] { get }
+    var singleFetch: @Sendable (_ withKey: FetchKey) async throws -> APIData { get }
+}
--- TCAWorkshop/Common/Network/GuessAgeClient.swift
+++ TCAWorkshop/Common/Network/GuessAgeClient.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+struct GuessAgeClient: APINetworkInterface {
+    var update: @Sendable (_ updateTarget: GuessAge) async throws -> Void
+    var fetchDataArray: @Sendable () async throws -> [GuessAge]
+    var singleFetch: @Sendable (String) async throws -> GuessAge
+}
+
+extension GuessAgeClient {
+    static let live = GuessAgeClient(
+        update: { _ in },
+        fetchDataArray: { [.testInstance()] },
+        singleFetch: { name in
+            let (data, response) = try await URLSession.shared.data(
+                from: URL(string: "https://api.agify.io?name=\(name)")!
+            )
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                print("Fetch Failed", response)
+                throw GuessAgeError.fetchError
+            }
+            
+            let result = try JSONDecoder().decode(
+                GuessAge.self,
+                from: data
+            )
+            
+            return result
+        }
+    )
+    
+    static let test = GuessAgeClient(
+        update: { _ in },
+        fetchDataArray: { [.testInstance()] },
+        singleFetch: { with in
+            //
+            return .testInstance()
+        }
+    )
+}
+
+enum GuessAgeError: Error {
+    case fetchError
+}
