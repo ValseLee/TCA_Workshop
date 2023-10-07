650ee34f0caa8fd25e40a46693f3f98d5fc871ce
valselee <sollleky72@gmail.com>
2023-10-07 15:48:37 +0900

    [Feat] 1. NavigationStack 을 기반으로 한 Root-Child 통합 구현 2. Child의 Action을 받고 Parent에 반영할 때, 필요한 action에 따라 분기처리 하는 코드 작성

--- TCAWorkshop/TCAWorkshop/GuessMy/GuessMyFeature.swift
+++ TCAWorkshop/TCAWorkshop/GuessMy/GuessMyFeature.swift
@@ -5,4 +5,56 @@
 //  Created by Celan on 2023/10/06.
 //
 
-import Foundation
+import ComposableArchitecture
+
+struct GuessMyFeature: Reducer {
+    struct State: Equatable {
+        var path = StackState<Path.State>()
+        var recentGuessMyAgeInformation: String?
+    }
+    enum Action: Equatable {
+        case path(StackAction<Path.State, Path.Action>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce<State, Action> { state, action in
+            switch action {
+            case let .path(.element(id: id, action: .guessMyAge(.guessAgeResponse(_)))):
+                guard let guessMyAgeState = state.path[id: id, case: /Path.State.guessMyAge]
+                else { return .none }
+                
+                if !guessMyAgeState.isGuessAgeIncorrect {
+                    state.recentGuessMyAgeInformation = guessMyAgeState.name
+                }
+                return .none
+            case .path:
+                return .none
+            }
+        }
+        // MARK: 모든 Stack 에 대해 부모-자식 간 통합 및 NavigationStack 설정을 진행하는 .forEach
+        .forEach(
+            \.path,
+             action: /Action.path
+        ) {
+            Path()
+        }
+    }
+    
+    struct Path: Reducer {
+        enum State: Equatable {
+            case guessMyAge(GuessMyAgeFeature.State)
+        }
+        enum Action: Equatable {
+            case guessMyAge(GuessMyAgeFeature.Action)
+        }
+        var body: some ReducerOf<Self> {
+            Scope(
+                state: /State.guessMyAge,
+                action: /Action.guessMyAge
+            ) {
+                GuessMyAgeFeature()
+            }
+        }
+    }
+
+}
